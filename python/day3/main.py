423eeb863a8508fa48e13e38144a051d65ceb310
Josh Jones <josh.jones@citizensadvice.org.uk>
2025-12-16 15:30:53 +0000

    DAY-3 part 2

--- python/day3/main.py
+++ python/day3/main.py
@@ -9,17 +9,39 @@ def main(input_file_name: str) -> int:
 
     bank_joltages: list[int] = []
     for bank in banks:
-        possible_joltages: list[int] = []
-        for i, left in enumerate(bank):
-            for right in bank[i + 1 :]:
-                possible_joltages.append(int(f"{left}{right}"))
-        bank_joltages.append(max(possible_joltages))
+        # Find the first digit of the max joltage
+        max_batt, idx = find_max_in_bank(bank[: ((len(bank) - 12) + 1)])
+        bank_joltage = str(max_batt)
+        # Find the rest of the digits of the max joltage from the remaining digits
+        for digits_left in range(11, 0, -1):
+            idx += 1
+            if digits_left == len(bank[idx:]):
+                bank_joltage += bank[idx:]
+                break
+            next_best, offset = find_max_in_bank(
+                bank[idx : (idx + ((len(bank[idx:]) - digits_left) + 1))]
+            )
+            idx += offset
+            bank_joltage += str(next_best)
+        bank_joltages.append(int(bank_joltage))
+
     return sum(bank_joltages)
+
+
+def find_max_in_bank(bank: str) -> tuple[int, int]:
+    max_value: int = -1
+    max_index: int = 0
+
+    for idx, batt in enumerate(bank):
+        if int(batt) > max_value:
+            max_value = int(batt)
+            max_index = idx
+    return max_value, max_index
 
 
 if __name__ == "__main__":
     print("### Test ###")
     result = main("test.txt")
-    print(f"Result: {result} ({'Pass' if result == 357 else 'Fail'})")
+    print(f"Result: {result} ({'Pass' if result == 3121910778619 else 'Fail'})")
     print("### Main ###")
     print(f"Result: {main('input.txt')}")
