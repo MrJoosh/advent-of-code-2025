15abf21a29f60c38297253875f11ea3599ed3723
Josh Jones <josh.jones@citizensadvice.org.uk>
2025-12-17 15:23:48 +0000

    DAY-6 part 2

--- python/day6/main.py
+++ python/day6/main.py
@@ -8,14 +8,38 @@ import math
 def main(input_file_name: str) -> int:
     with open(input_file_name, "r", encoding="utf-8") as input_file:
         input_data_raw: list[str] = input_file.read().splitlines()
-    # Split the data into columns by whitespace
-    input_data: list[list[str]] = [line.split() for line in input_data_raw]
 
     # Transpose the data into columnar lists
-    input_columnar: list[list[str]] = list(map(list, zip(*input_data)))
+    input_columnar: list[list[str]] = list(map(list, zip(*input_data_raw)))
+
+    problems: list[list[str]] = []
+    column_buffer: list[str] = []
+    # Loop over the columns, right to left
+    for line in reversed(input_columnar):
+        # Check if the current column is a spaces column, if so output the buffer
+        if all(val == " " for val in line):
+            problems.append(column_buffer)
+            column_buffer = []
+            continue
+
+        # Grab the operand if it's there
+        operand = line.pop()
+
+        # Merge the digits in the line
+        value: str = ""
+        for val in line:
+            if val != " ":
+                value += val
+
+        column_buffer.append(value)
+        if operand != " ":
+            column_buffer.append(operand)
+
+    # Append the last problem as no space column
+    problems.append(column_buffer)
 
     problem_results: list[int] = []
-    for problem in input_columnar:
+    for problem in problems:
         operand = problem.pop()
         problem_results.append(calculate_problem(operand, *problem))
 
@@ -42,6 +66,6 @@ def calculate_problem(operand: str, *values: str) -> int:
 if __name__ == "__main__":
     print("### Test ###")
     result = main("test.txt")
-    print(f"Result: {result} ({'Pass' if result == 4277556 else 'Fail'})")
+    print(f"Result: {result} ({'Pass' if result == 3263827 else 'Fail'})")
     print("### Main ###")
     print(f"Result: {main('input.txt')}")
