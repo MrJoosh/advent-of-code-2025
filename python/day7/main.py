d7aae1b9ef3817bb1c6a7d57f73d98533aeb11be
Josh Jones <josh.jones@citizensadvice.org.uk>
2025-12-18 15:50:43 +0000

    DAY-7 part 2

--- python/day7/main.py
+++ python/day7/main.py
@@ -2,37 +2,35 @@
 Python implementation of the solution for day 7 of Advent of Code 2025
 """
 
-from collections import deque
+from collections import Counter
 
 
 def main(input_file_name: str) -> int:
     with open(input_file_name, "r", encoding="utf-8") as input_file:
-        input_data_raw: deque[str] = deque(input_file.read().splitlines())
+        input_data_raw: list[str] = input_file.read().splitlines()
 
-    beams: list[int] = []  # List of indices that have a beam
+    # Part 2 solution by /u/mnvrth (https://github.com/mnvr/aoc-25/blob/main/07.py)
+    paths: Counter = Counter()  # outner object of paths taken
     splits: int = 0  # Number of times the beam has split
-    while True:
-        try:
-            row: str = input_data_raw.popleft()
-        except IndexError:
-            break
-        if not beams:
-            beams.append(row.find("S"))
-            continue
-        new_beams: list[int] = list(beams)
-        for beam in beams:
-            if row[beam] == "^":
-                new_beams.remove(beam)
-                new_beams.append(beam - 1)
-                new_beams.append(beam + 1)
-                splits += 1
-        beams = list(set(new_beams))
-    return splits
+
+    for line in input_data_raw:
+        for i, c in enumerate(line):
+            match c:
+                case "S":
+                    paths[i] = 1
+                case "^":
+                    if i in paths:
+                        splits += 1
+                        paths[i - 1] += paths[i]
+                        paths[i + 1] += paths[i]
+                        del paths[i]
+
+    return paths.total()
 
 
 if __name__ == "__main__":
     print("### Test ###")
     result = main("test.txt")
-    print(f"Result: {result} ({'Pass' if result == 21 else 'Fail'})")
+    print(f"Result: {result} ({'Pass' if result == 40 else 'Fail'})")
     print("### Main ###")
     print(f"Result: {main('input.txt')}")
